42b572be6a5074738f77ec30639054c5a8e76037
Mit Mehta <mehta.mit09@gmail.com>
2015-03-12 18:12:42 -0400

    IoC

---
+++ public/javascript/angular/021-1.js
@@ -0,0 +1,40 @@
+﻿var app = angular.module("HelloWorldApp", []);
+
+app.controller("HelloWorldController", function ($scope, $http, PlayerService) {
+    $scope.msg = 'Hello from controller';
+
+    $scope.addUser = function (player) {
+        PlayerService.addPlayer(player, function (response) {
+           $scope.players = response;
+        });
+       $scope.player = new Object();
+    }
+
+    $scope.getDev = function () {
+        PlayerService.getAllPlayer(function (response) {
+            $scope.players = response
+            console.log($scope.players);
+        });
+    }
+
+    $scope.delete = function (id) {
+        PlayerService.deletePlayer(id, function (response) {
+            $scope.players = response;
+        });
+    }
+
+    $scope.selectedIndex = null;
+    $scope.edit = function (id) {
+        PlayerService.getPlayerById(id, function (response) {
+            $scope.player = response;
+        });
+        $scope.selectedIndex = id;
+    }
+
+    $scope.updateUser = function () {
+        PlayerService.updatePlayer($scope.selectedIndex, $scope.player, function (response) {
+            $scope.players = response;
+        });
+        $scope.player = new Object();
+    }
+});
---
+++ public/javascript/angular/service/PlayerService.js
@@ -0,0 +1,37 @@
+﻿app.factory('PlayerService', function($http){
+    var addPlayer = function(player, callback){
+        $http.post("/player" ,player)
+       .success(callback);
+    }
+
+    var getAllPlayer = function (callback) {
+        $http.get('/player')
+        .success(callback);
+    }
+
+    var deletePlayer = function (id, callback) {
+        $http.delete('/player/' + id)
+        .success(callback);
+    }
+
+    var getPlayerById = function (id, callback) {
+        $http.get('/player/' + id)
+        .success(callback);
+    }
+
+    var updatePlayer = function (id, updatedPlayer, callback) {
+        $http.put('/player/' + id, updatedPlayer)
+        .success(callback);
+    }
+
+    return{
+        addPlayer: addPlayer,
+        getAllPlayer: getAllPlayer,
+        deletePlayer: deletePlayer,
+        getPlayerById: getPlayerById,
+        updatePlayer: updatePlayer
+    };
+
+    
+
+});
